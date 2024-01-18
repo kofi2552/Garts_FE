60f0573a8f907ed8d6839a40af410e00d8ecfb49
kofi2552 <kofiloyke2552@gmail.com>
2024-01-18 23:46:43 +0000

    Phase 3 update payment confirmation

--- src/utils/newRequest.js
+++ src/utils/newRequest.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  // baseURL: import.meta.env.VITE_BASE_URL,
-  baseURL: import.meta.env.VITE_SERVER_URL,
+  baseURL: import.meta.env.VITE_BASE_URL,
+  // baseURL: import.meta.env.VITE_SERVER_URL,
   withCredentials: true,
 });
 
